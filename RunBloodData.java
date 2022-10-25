46100dc62eb15f2d5b85a6b3aa0fc21e6b041610
Elyka Anciado <116554532+ely2474@users.noreply.github.com>
2022-10-25 08:48:19 +0800

    Add files via upload

---
+++ RunBloodData.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class RunBloodData{
+public static void main(String[] args){
+Scanner scn = new Scanner(System.in);
+System.out.print("Enter blood type of patient: ");
+String input1 = scn.nextLine();
+
+System.out.print("Enter the Rhesus factor (+ or -): ");
+String input2 = scn.nextLine();
+
+if(input1.equals("O")){
+BloodData bd = new BloodData(input1,input2);
+if(input2.equals("+")){
+bd.display();
+}
+else if(input2.equals("-")){
+bd.display();
+}
+else{
+System.out.println("You entered wrong input!");
+}
+}
+else if(input1.isEmpty()){
+BloodData bd = new BloodData();
+if(input2.isEmpty()){
+bd.display();
+
+}
+else{
+System.out.println("You entered wrong input!");
+}
+
+}
+
+
+else if(input1.equals("A") ){
+BloodData bd = new BloodData(input1,input2);
+if(input2.equals("+")){
+bd.display();
+}
+else if(input2.equals("-")){
+bd.display();
+}
+else{
+System.out.println("You entered wrong input!");
+}
+}
+
+else if(input1.equals("B")){
+BloodData bd = new BloodData(input1,input2);
+if(input2.equals("+")){
+bd.display();
+}
+else if(input2.equals("-")){
+bd.display();
+}
+else{
+System.out.println("You entered wrong input!");
+}
+}
+else if(input1.equals("AB")){
+BloodData bd = new BloodData(input1,input2);
+if(input2.equals("+")){
+
+bd.display();
+}
+else if(input2.equals("-")){
+
+bd.display();
+}
+else{
+System.out.println("You entered wrong input!");
+}
+
+}
+
+
+}
+
+}
